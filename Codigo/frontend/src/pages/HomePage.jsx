87ff4218ca15d83118df22dfd1d763803fa6cb63
Erick Guedes <erickguedes0515@gmail.com>
2026-04-27 01:23:59 -0300

    feat: implementa layout da HomePage e estilização base

--- Codigo/frontend/src/pages/HomePage.jsx
+++ Codigo/frontend/src/pages/HomePage.jsx
@@ -1,20 +1,20 @@
+import '../styles/home.css'
+
 import Navbar from '../components/homepage/Navbar'
-import Footer from '../components/homepage/Footer'
 import HeroSection from '../components/homepage/HeroSection'
 import AboutSection from '../components/homepage/AboutSection'
 import PortfolioSection from '../components/homepage/PortfolioSection'
 import ContactSection from '../components/homepage/ContactSection'
+import Footer from '../components/homepage/Footer'
 
 export default function HomePage() {
   return (
     <>
       <Navbar />
-      <main>
-        <HeroSection />
-        <AboutSection />
-        <PortfolioSection />
-        <ContactSection />
-      </main>
+      <HeroSection />
+      <AboutSection />
+      <PortfolioSection />
+      <ContactSection />
       <Footer />
     </>
   )
